5d96520a72f6e109523c7647336a717f9083c458
Aditya Ghosh <adityaghosh5025@gmail.com>
2024-03-16 12:31:11 +0530

    day4 done

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
     // TODO: pass in cmdline args to set day
     public static void main(String[] args) throws IOException {
         List<String> lines =  readFile("input_data/day_4.txt");
-        int answer = Day_4.part_1(lines);
+        int answer = Day_4.part_2(lines);
         System.out.println(answer);
     }
 }
